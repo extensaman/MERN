ba9def918f1c770727fca639e1b17fcf96113b1e
verus <verus.weddding@gmail.com>
2025-05-16 18:55:15 +0300

    Yup added

--- task01-formik/src/App.js
+++ task01-formik/src/App.js
@@ -1,14 +1,26 @@
 import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 import './App.css';
 
 function App() {
-  initialValues = { email: "", password: "" };
+  const initialValues = { email: "", password: "" };
   const onSubmit = (values) => console.log(values);
+  const validateSchema = Yup.object({
+    email: Yup.string().email("Неверный формат email").required("Поле email обязательно"),
+    password: Yup.string().required("Поле email обязательно"),
+  });
 
   return (
     <div className="App">
       <Formik initialValues={initialValues} onSubmit={onSubmit}>
-
+        {({ values }) => (
+          <Form>
+            <Field name="email" type="email"></Field>
+            <Field name="password" type="password"></Field>
+            <button type="submit">Enter</button>
+            <div>{values.email}</div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
