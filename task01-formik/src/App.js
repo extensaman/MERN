b47acbc5736e9c7954a5403c55ccb78bb81ad7ac
verus <verus.weddding@gmail.com>
2025-05-18 13:22:54 +0300

    styles added for form

--- task01-formik/src/App.js
+++ task01-formik/src/App.js
@@ -1,28 +1,29 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import './App.css';
 
 function App() {
   const initialValues = { email: "", password: "" };
   const onSubmit = (values) => console.log(values);
   const validateSchema = Yup.object({
     email: Yup.string().email("Неверный формат email").required("Поле email обязательно"),
-    password: Yup.string().required("Поле email обязательно"),
+    password: Yup.string().required("Поле password обязательно"),
   });
 
   return (
-    <div className="App">
-      <Formik initialValues={initialValues} onSubmit={onSubmit}>
+    <div>
+      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validateSchema}>
         {({ values }) => (
           <Form>
-            <Field name="email" type="email"></Field>
-            <Field name="password" type="password"></Field>
+            <Field name="email" type="text"></Field>
+            <div className='contentBlock'><ErrorMessage name="email" /></div>
+            <Field style={{ height: 14 }} name="password" type="password"></Field>
+            <div className='contentBlock'><ErrorMessage name="password" /></div>
             <button type="submit">Enter</button>
             <div>{values.email}</div>
           </Form>
         )}
       </Formik>
-    </div>
+    </div >
   );
 }
 
