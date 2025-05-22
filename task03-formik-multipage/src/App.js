fd99714d469178cb35bca7da255a01f692455b1f
verus <verus.wedding@gmail.com>
2025-05-22 17:04:43 +0300

    Done

--- task03-formik-multipage/src/App.js
+++ task03-formik-multipage/src/App.js
@@ -1,11 +1,10 @@
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-import { useState } from 'react';
-import ChooseForm from './components/ChooseForm';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { useState } from "react";
+import ChooseForm from "./components/ChooseForm";
 
 function App() {
-
-  const { currentPage, setCurrentPage } = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
 
   const initialValues = {
     name: "",
@@ -14,33 +13,49 @@ function App() {
     street: "",
     house: "",
     card: "",
-    expire: ""
+    expire: "",
   };
 
-  const pages = ["Name and surname", "Adress", "Payment information"];
+  const pages = ["Name and surname", "Address", "Payment information"];
+
+  const nextPage = () => {
+    if (currentPage < pages.length - 1) setCurrentPage(currentPage + 1);
+  };
 
   const onSubmit = (values) => console.log(values);
 
   const validateSchema = Yup.object({
-    email: Yup.string().email("Неверный формат email").required("Поле email обязательно"),
+    email: Yup.string()
+      .email("Неверный формат email")
+      .required("Поле email обязательно"),
     password: Yup.string().required("Поле password обязательно"),
   });
 
   return (
     <div>
-      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validateSchema}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validateSchema}
+      >
         {({ values }) => {
           return (
             <Form>
-              {console.log(currentPage)}
+              <h1>{pages[currentPage]}</h1>
               <ChooseForm page={currentPage} />
+              <button
+                onClick={nextPage}
+                disabled={currentPage === pages.length - 1}
+              >
+                Next page
+              </button>
               <button type="submit">Enter</button>
               <div>{values.email}</div>
             </Form>
-          )
+          );
         }}
       </Formik>
-    </div >
+    </div>
   );
 }
 
--- task03-formik-multipage/src/components/ChooseForm.js
+++ task03-formik-multipage/src/components/ChooseForm.js
@@ -1,14 +1,18 @@
 import AdressForm from "./AdressForm";
-import FioForm from "./fioForm"
+import FioForm from "./fioForm";
 import PayForm from "./PayForm";
 
-const ChooseForm = (page) => {
-    switch (page) {
-        case 0: return <FioForm />;
-        case 1: return <AdressForm />;
-        case 2: return <PayForm />
-        default: return null;
-    }
-}
+const ChooseForm = (props) => {
+  switch (props.page) {
+    case 0:
+      return <FioForm />;
+    case 1:
+      return <AdressForm />;
+    case 2:
+      return <PayForm />;
+    default:
+      return null;
+  }
+};
 
-export default ChooseForm;  
+export default ChooseForm;
