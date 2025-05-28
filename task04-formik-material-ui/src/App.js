60cc373c5564c732652395cbf5a55aa6c77896d3
verus <verus.wedding@gmail.com>
2025-05-28 16:47:40 +0300

    grid added

--- task04-formik-material-ui/src/App.js
+++ task04-formik-material-ui/src/App.js
@@ -1,7 +1,7 @@
 import { useFormik } from "formik";
 import Button from "@mui/material/Button";
 import * as Yup from "yup";
-import { TextField } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 
 function App() {
   const initialValues = { email: "", password: "", confirmPassword: "" };
@@ -27,51 +27,63 @@ function App() {
   });
 
   return (
-    <div>
-      <form onSubmit={formik.handleSubmit}>
-        <TextField
-          size="small"
-          id="email"
-          name="email"
-          label="Введите адрес электронной почты"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.errors.email && formik.touched.email}
-          helperText={formik.touched.email && formik.errors.email}
-        />
-        <TextField
-          size="small"
-          id="password"
-          name="password"
-          type="password"
-          label="Введите пароль"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={formik.errors.password && formik.touched.password}
-          helperText={formik.touched.password && formik.errors.password}
-        />
+    <Grid container spacing={2}>
+      <Grid size={5} />
+      <Grid size={2}>
+        <form onSubmit={formik.handleSubmit}>
+          <TextField
+            fullWidth
+            sx={{ my: "10px" }}
+            size="small"
+            id="email"
+            name="email"
+            label="Введите адрес электронной почты"
+            value={formik.values.email}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.errors.email && formik.touched.email}
+            helperText={formik.touched.email && formik.errors.email}
+          />
+          <TextField
+            fullWidth
+            sx={{ my: "10px" }}
+            size="small"
+            id="password"
+            name="password"
+            type="password"
+            label="Введите пароль"
+            value={formik.values.password}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={formik.errors.password && formik.touched.password}
+            helperText={formik.touched.password && formik.errors.password}
+          />
 
-        <TextField
-          size="small"
-          id="confirmPassword"
-          name="confirmPassword"
-          type="password"
-          label="Повторите пароль"
-          value={formik.values.confirmPassword}
-          onChange={formik.handleChange}
-          onBlur={formik.handleBlur}
-          error={
-            formik.errors.confirmPassword && formik.touched.confirmPassword
-          }
-          helperText={
-            formik.touched.confirmPassword && formik.errors.confirmPassword
-          }
-        />
-        <Button variant="outlined">Hello, MUI</Button>
-      </form>
-    </div>
+          <TextField
+            fullWidth
+            sx={{ my: "10px" }}
+            size="small"
+            id="confirmPassword"
+            name="confirmPassword"
+            type="password"
+            label="Повторите пароль"
+            value={formik.values.confirmPassword}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            error={
+              formik.errors.confirmPassword && formik.touched.confirmPassword
+            }
+            helperText={
+              formik.touched.confirmPassword && formik.errors.confirmPassword
+            }
+          />
+          <Button variant="outlined" fullWidth>
+            Hello, MUI
+          </Button>
+        </form>
+      </Grid>
+      <Grid size={5} />
+    </Grid>
   );
 }
 
