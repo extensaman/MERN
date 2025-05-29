4b72aa7eaa87b830521d6b4f4ca816008e9e8b5f
verus <verus.weddding@gmail.com>
2025-05-29 11:53:33 +0300

    NodeMailer in task5 created

---
+++ task05-nodemailer/index.js
@@ -0,0 +1,20 @@
+const nodeMailer = require("nodemailer");
+
+const transporter = nodeMailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'XXX',
+        pass: 'XXX'
+    }
+});
+
+transporter.sendMail({
+    from: 'Node.js <verus>',
+    to: 'XXX',
+    subject: 'Message fron Node.js',
+    text: 'This message was sent from Node js server.',
+    html: 'This <i>message</i> was sent from <strong>Node js</strong> server.'
+})
+    .then(result => console.log(result))
+    .catch(err => console.log(err));
+console.log("Finished");
