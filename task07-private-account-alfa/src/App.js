d071ce568bfd43e086bfc995cf92af7e58d2d71e
verus <verus.wedding@gmail.com>
2025-06-16 13:13:06 +0300

    task07 created

---
+++ task07-private-account-alfa/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <h1>Hello</h1>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
