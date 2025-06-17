b8625fd34f78dcbe175d5e32ade8a8186d3014d8
verus <verus.wedding@gmail.com>
2025-06-17 17:01:13 +0300

    Good

--- task07-private-account-alfa/src/components/PrivateAccountLoader.js
+++ task07-private-account-alfa/src/components/PrivateAccountLoader.js
@@ -1,17 +1,37 @@
 import React from "react";
-import { TOKEN_KEY_NAME_IN_LOCAL_STORAGE } from "../constants";
+import axios from "axios";
+import { BACK_URL, TOKEN_KEY_NAME_IN_LOCAL_STORAGE } from "../constants";
 
 class PrivateAccountLoader extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isLoading: false,
+      isLoading: true,
       data: null,
     };
 
     const tokenFromLocalStorage = localStorage.getItem(
       TOKEN_KEY_NAME_IN_LOCAL_STORAGE
     );
+
+    axios
+      .post(BACK_URL, { token: tokenFromLocalStorage })
+      .then((response) => {
+        console.log(response);
+        this.setState({ isLoading: false, data: "FROM BACK" });
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(
+            "Есть связь с сервером, но код ответа от него следующий:" +
+              err.response.status
+          );
+        } else {
+          console.log("Нет связи с сервером");
+        }
+
+        this.setState({ isLoading: false, data: null });
+      });
   }
 
   render() {
