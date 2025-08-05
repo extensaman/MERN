f2a6b14f6c51477706297a24beba95749ccd9b9f
verus <verus.wedding@gmail.com>
2025-08-05 16:40:09 +0300

    Flash session added

--- task08-express-web/source/middleware.js
+++ task08-express-web/source/middleware.js
@@ -1,8 +1,10 @@
 import { matchedData, validationResult } from "express-validator";
 
-export const handleErrors = (req, res, next) => {
+export const handleErrors = async (req, res, next) => {
   const result = validationResult(req);
   if (!result.isEmpty()) {
+    await req.flash("errors", result.mapped());
+    await req.flash("body", req.body);
     const backURL = req.header("Referer") || "/";
     res.redirect(backURL);
   } else {
@@ -14,3 +16,17 @@ export const putRequestToContext = (req, res, next) => {
   res.locals.req = req;
   next();
 };
+
+export function extendFlashAPI(req, _, next) {
+  req.getFlash = async function (name) {
+    const d = await this.consumeFlash(name);
+    return d.length > 0 ? d[0] : undefined;
+  };
+  next();
+}
+
+export async function getErrors(req, res, next) {
+  res.locals.errors = (await req.getFlash("errors")) || {};
+  res.locals.body = (await req.getFlash("body")) || {};
+  next();
+}
--- task08-express-web/source/router.js
+++ task08-express-web/source/router.js
@@ -2,7 +2,12 @@ import { Router } from "express";
 import { urlencoded } from "express";
 import methodOverride from "method-override";
 import { logger } from "./utility.js";
-import { putRequestToContext, handleErrors } from "./middleware.js";
+import {
+  putRequestToContext,
+  handleErrors,
+  extendFlashAPI,
+  getErrors,
+} from "./middleware.js";
 import { todoV } from "./validators.js";
 import cookieParser from "cookie-parser";
 import {
@@ -15,15 +20,43 @@ import {
   setOrder,
 } from "./controllers/todos.js";
 import { error500Handler, mainErrorHandler } from "./error-handlers.js";
+import session from "express-session";
+import _FileStore from "session-file-store";
+import { flash } from "express-flash-message";
 
 const router = Router();
+const FileStrore = _FileStore(session);
 
 router.use(urlencoded({ extended: true })); // чтобы была возможность принимать POST-запросы (по дефолту только GET-запросы принимаются)
 router.use(methodOverride("_method")); // для возможности использования запросов PUT и DELETE в формах, т.к. по дефолту формы могут посылать только GET и POST запросы
 router.use(cookieParser());
+router.use(
+  // привяка посредника, обрабатывающего сессии
+  session({
+    // определение места хранения сессий (файловое хранилище)
+    store: new FileStrore({
+      path: "./storage/sessions",
+      reapAsync: true,
+      reapSyncFallback: true,
+      fallbackSessionFn: () => {
+        return {};
+      },
+      logFn: () => {},
+    }),
+    secret: "abcdefgh",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60,
+    },
+  })
+);
+// привяка посредника, который с использованием привязанного выше посредника , обрабатывающего сессии, будет обрабатывать всплывающие сообщения
+router.use(flash({ sessionKeyName: "flash-message" }));
+router.use(extendFlashAPI);
 router.use(logger);
 router.use(putRequestToContext);
-router.get("/add", addItem);
+router.get("/add", getErrors, addItem);
 router.get("/:id", detailPage);
 router.get("/", mainPage);
 router.post("/add", todoV, handleErrors, add);
